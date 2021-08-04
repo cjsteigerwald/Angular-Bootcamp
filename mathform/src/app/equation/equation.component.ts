6aa9e6ee4dddd5198ac2f43efe6537f784c6884a
Christopher Steigerwald <cjsteigerwald@gmail.com>
2021-08-04 06:37:53 -0700

    Finish the app

--- mathform/src/app/equation/equation.component.ts
+++ mathform/src/app/equation/equation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { delay, filter,scan } from 'rxjs/operators';
 import { MathValidators } from '../math-validators';
 
 @Component({
@@ -8,6 +9,7 @@ import { MathValidators } from '../math-validators';
   styleUrls: ['./equation.component.css']
 })
 export class EquationComponent implements OnInit {
+  secondsPerSolution = 0;
   mathForm = new FormGroup({
     a: new FormControl(this.randomNumber()),
     b: new FormControl(this.randomNumber()),
@@ -27,6 +29,26 @@ export class EquationComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    
+    this.mathForm.statusChanges.pipe(
+      filter(value => value === 'VALID'),
+      delay(300),
+      scan((acc) => {
+        return {
+          numberSolved: acc.numberSolved + 1,
+          startTime: acc.startTime
+        }
+      }, { numberSolved: 0, startTime: new Date() })
+    ).subscribe(({ numberSolved, startTime }) => {
+      this.secondsPerSolution = (        
+        new Date().getTime() - startTime.getTime()
+      ) / numberSolved / 1000;
+      this.mathForm.patchValue({
+        a: this.randomNumber(),
+        b: this.randomNumber(),
+        answer: ''
+      });
+    });
   }
 
 
