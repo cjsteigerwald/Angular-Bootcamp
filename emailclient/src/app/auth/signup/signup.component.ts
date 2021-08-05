3942c3f058c380728e59b219e837a1cd2f6ee5a5
Christopher Steigerwald <cjsteigerwald@gmail.com>
2021-08-05 00:04:31 -0700

    Adding validators

--- emailclient/src/app/auth/signup/signup.component.ts
+++ emailclient/src/app/auth/signup/signup.component.ts
@@ -1,4 +1,9 @@
+
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatchPassword } from '../validators/match-password';
+import { UniqueUsername } from '../validators/unique-username';
+
 
 @Component({
   selector: 'app-signup',
@@ -6,8 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  authForm = new FormGroup({
+    username: new FormControl('',[
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.pattern(/^[a-z0-9]+$/)
+    ], [this.uniqureUsername.validate]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20),
+    ]),
+    passwordConfirmation: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20),
+    ]),    
+  }, { validators: [this.matchpassword.validate] });
 
-  constructor() { }
+  constructor(
+    private matchpassword: MatchPassword, 
+    private uniqureUsername: UniqueUsername) { }
 
   ngOnInit(): void {
   }
---
+++ emailclient/src/app/auth/validators/unique-username.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import { AsyncValidator, FormControl } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import { map, catchError } from "rxjs/operators";
+import { of } from "rxjs";
+
+@Injectable({ providedIn: 'root' })
+export class UniqueUsername implements AsyncValidator{
+  constructor(private http: HttpClient) {}
+  validate = (control: FormControl) => {
+    const  { value } = control;
+    console.log(this.http);
+
+    return this.http.post<any>('https://api.angular-email.com/auth/username', {
+      username: value
+    }).pipe(
+      map((value) => {
+        if (value.available){
+          return null;
+        }
+      }),
+      catchError((err) => {
+        if (err.error.username) {
+          return of({ nonUniqueUsername: true});
+        } else {
+          return of ({ noConnection: true });
+        }
+
+      })
+    );
+  }
+  
+}
