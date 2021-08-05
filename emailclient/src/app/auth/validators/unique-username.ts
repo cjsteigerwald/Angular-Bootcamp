99ceb9d0aad09cfc8ede4fe29d144ed1d3dab192
Christopher Steigerwald <cjsteigerwald@gmail.com>
2021-08-05 00:17:17 -0700

    Adding auth.service for http username available request

--- emailclient/src/app/auth/validators/unique-username.ts
+++ emailclient/src/app/auth/validators/unique-username.ts
@@ -1,19 +1,16 @@
+import { AuthService } from './../auth.service';
 import { Injectable } from "@angular/core";
 import { AsyncValidator, FormControl } from "@angular/forms";
-import { HttpClient } from "@angular/common/http";
 import { map, catchError } from "rxjs/operators";
 import { of } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class UniqueUsername implements AsyncValidator{
-  constructor(private http: HttpClient) {}
+  constructor(private authService: AuthService) {}
   validate = (control: FormControl) => {
     const  { value } = control;
-    console.log(this.http);
 
-    return this.http.post<any>('https://api.angular-email.com/auth/username', {
-      username: value
-    }).pipe(
+    return this.authService.userNameAvailable(value).pipe(
       map((value) => {
         if (value.available){
           return null;
