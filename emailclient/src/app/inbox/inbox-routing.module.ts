50adf2750c4cda2ff1115bc5f2344a11c420cf65
Christopher Steigerwald <cjsteigerwald@gmail.com>
2021-08-09 03:09:21 -0700

    Adding not-found component

--- emailclient/src/app/inbox/inbox-routing.module.ts
+++ emailclient/src/app/inbox/inbox-routing.module.ts
@@ -4,12 +4,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EmailResolverService } from './email-resolver.service';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { 
     path: '', 
     component: HomeComponent,
     children: [
+      {
+        path: 'not-found',
+        component: NotFoundComponent
+      },
       { 
         path: ':id', 
         component: EmailShowComponent,
